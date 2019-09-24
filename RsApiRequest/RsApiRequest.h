2f1e4d0870a52dcc2ca57c1da29a7752eb4b14c0
Sathish Kumar <sathishkumar.rs@payoda.com>
2019-09-24 13:57:31 +0530

    Initial Commit

---
+++ RsApiRequest/RsApiRequest.h
@@ -0,0 +1,19 @@
+//
+//  RsApiRequest.h
+//  RsApiRequest
+//
+//  Created by sathish on 9/24/19.
+//  Copyright © 2019 payoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RsApiRequest.
+FOUNDATION_EXPORT double RsApiRequestVersionNumber;
+
+//! Project version string for RsApiRequest.
+FOUNDATION_EXPORT const unsigned char RsApiRequestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RsApiRequest/PublicHeader.h>
+
+
